2394d0ae6617afdbbd87f6b7d6eeef8e8c502fd7
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-07-03 17:20:13 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import FloatingWhatsApp from './components/FloatingWhatsApp';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+      <FloatingWhatsApp />
+    </>
+  );
+}
